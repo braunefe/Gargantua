85ae1c07fd9230d2ceedeeeb6ae2ed9983333813
Christrian Buck <cbuck@lantis.de>
2015-10-26 01:54:48 +0000

    added checks in Cell

--- src/Cell.h
+++ src/Cell.h
@@ -1,26 +1,27 @@
 #ifndef CELL_H_
 #define CELL_H_
 
-#include <utility> 
+#include <utility>
 
 using namespace std;
 
-class Cell //cells of the distance matrix of the DP procedure
-{
-	private:
-	
-	long double distance;
-	pair<int,int> previous;
+class Cell  // cells of the distance matrix of the DP procedure
+    {
+ private:
+  long double distance;
+  pair<int, int> previous;
+  bool initialized;
 
-	public:
-	Cell();
-	Cell(long double d, pair<int,int> p);
-	
-	long double getDistance();
-	void setDistance(long double d);
-	
-	pair<int,int> getPrevious();
-	void setPrevious (pair<int,int> p);
+ public:
+  Cell() : distance(0), initialized(false) {};
+  Cell(long double d, pair<int, int> p)
+      : distance(d), previous(p), initialized(true){};
+
+  long double getDistance() const;
+  void setDistance(long double d);
+
+  pair<int, int> getPrevious() const;
+  void setPrevious(pair<int, int> p);
 };
 
 #endif /*CELL_H_*/
--- src/cell.cpp
+++ src/cell.cpp
@@ -1,28 +1,23 @@
-#include <utility> 
+#include <utility>
+#include <cassert>
 #include "Cell.h"
 
-Cell::Cell(long double d, pair<int,int> p)
-{
-	setDistance(d);
-	setPrevious(p);
+long double Cell::getDistance() const {
+  assert(initialized);
+  return distance;
 }
 
-long double Cell::getDistance()
-{
-	return distance;	
+void Cell::setDistance(long double d) {
+  assert(initialized);
+  distance = d;
 }
 
-void Cell::setDistance(long double d)
-{
-	distance = d;
+pair<int, int> Cell::getPrevious() const {
+  assert(initialized);
+  return previous;
 }
 
-pair<int,int> Cell::getPrevious()
-{
-	return previous;
-}
-
-void Cell::setPrevious(pair<int,int> p)
-{
-	previous = p;
+void Cell::setPrevious(pair<int, int> p) {
+  previous = p;
+  initialized = true;
 }
